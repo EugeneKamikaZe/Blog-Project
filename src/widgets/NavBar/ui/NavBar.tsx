e5699be26fe2902ceb487dcf341bb85d609acfb9
Eugene <egor_egorov_qwerty@mail.ru>
2023-02-19 14:38:53 +0300

    add modal

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Buton/Button';
+import { useTranslation } from 'react-i18next';
 import s from './NavBar.module.scss';
 
 interface NavBarProps {
     className?: string
 }
 
-export const NavBar = ({ className }: NavBarProps) => (
-    <div className={classNames(s.NavBar, {}, [className])}>
-        <div className={s.links}>
-            /
+export const NavBar = ({ className }: NavBarProps) => {
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
+    return (
+        <div className={classNames(s.Navbar, {}, [className])}>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={s.links}
+                onClick={onToggleModal}
+            >
+                {t('sign_in')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line */}
+                {t('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.')}
+            </Modal>
         </div>
-    </div>
-);
+    );
+};
